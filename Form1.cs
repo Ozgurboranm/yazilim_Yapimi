8245a98c4212f66a051eac0c88d3871d7bc1354e
Özgür <ozgurboranm@gmail.com>
2023-06-04 12:28:45 +0300

    takvim arayüzü tasarlandı
    takvim arayüzü tasarlandı

--- Form1.cs
+++ Form1.cs
@@ -64,7 +64,7 @@ namespace yazilim_Yapimi
             {
                 month = 12;
                 year--;
-            }//ohh yehah
+            }
             String monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
             lbdate.Text = monthname + " " + year;
 
