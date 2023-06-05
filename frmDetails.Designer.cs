f53a4d6da7b811cf66c843a719e4169c66952224
Özgür <ozgurboranm@gmail.com>
2023-06-05 13:16:14 +0300

    update & delete & save

--- frmDetails.Designer.cs
+++ frmDetails.Designer.cs
@@ -38,19 +38,33 @@
             this.tbl_EventTableAdapter = new yazilim_Yapimi.yazilim_YapimiDataSetTableAdapters.tbl_EventTableAdapter();
             this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.eventDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEvent = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCean = new System.Windows.Forms.Button();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.yazilim_YapimiDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblEventBindingSource)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(20, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 24);
+            this.button1.Size = new System.Drawing.Size(115, 42);
             this.button1.TabIndex = 12;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -58,12 +72,14 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 85);
+            this.groupBox1.Location = new System.Drawing.Point(1038, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(252, 429);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Size = new System.Drawing.Size(267, 372);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Events";
             // 
             // dataGridView1
             // 
@@ -74,10 +90,12 @@
             this.eventDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.tblEventBindingSource;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Location = new System.Drawing.Point(5, 29);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(246, 410);
+            this.dataGridView1.Size = new System.Drawing.Size(257, 338);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // yazilim_YapimiDataSet
             // 
@@ -105,14 +123,127 @@
             this.eventDataGridViewTextBoxColumn.HeaderText = "Event";
             this.eventDataGridViewTextBoxColumn.Name = "eventDataGridViewTextBoxColumn";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 23);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Event:";
+            // 
+            // tbEvent
+            // 
+            this.tbEvent.Location = new System.Drawing.Point(94, 78);
+            this.tbEvent.Name = "tbEvent";
+            this.tbEvent.Size = new System.Drawing.Size(331, 31);
+            this.tbEvent.TabIndex = 17;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbDate);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tbEvent);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(20, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(435, 137);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Event Add";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnUpdate);
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnCean);
+            this.groupBox3.Controls.Add(this.btnSave);
+            this.groupBox3.Controls.Add(this.btnList);
+            this.groupBox3.Location = new System.Drawing.Point(533, 109);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(174, 279);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tasks";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(6, 30);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(84, 29);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(6, 80);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 29);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCean
+            // 
+            this.btnCean.Location = new System.Drawing.Point(6, 126);
+            this.btnCean.Name = "btnCean";
+            this.btnCean.Size = new System.Drawing.Size(84, 29);
+            this.btnCean.TabIndex = 2;
+            this.btnCean.Text = "Clean";
+            this.btnCean.UseVisualStyleBackColor = true;
+            this.btnCean.Click += new System.EventHandler(this.btnCean_Click);
+            // 
+            // tbDate
+            // 
+            this.tbDate.Location = new System.Drawing.Point(94, 28);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(331, 31);
+            this.tbDate.TabIndex = 18;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(6, 170);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(84, 29);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(6, 214);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(84, 29);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
             // frmDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(471, 526);
+            this.ClientSize = new System.Drawing.Size(1332, 683);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "frmDetails";
             this.Text = "frmDetails";
             this.Load += new System.EventHandler(this.frmDetails_Load);
@@ -120,6 +251,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.yazilim_YapimiDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblEventBindingSource)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,5 +268,16 @@
         private yazilim_YapimiDataSetTableAdapters.tbl_EventTableAdapter tbl_EventTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn eventDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEvent;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnCean;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
--- frmDetails.cs
+++ frmDetails.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace yazilim_Yapimi
 {
@@ -23,12 +24,59 @@ namespace yazilim_Yapimi
             calendar.Show();
             this.Hide();
         }
-
+        SqlConnection connection = new SqlConnection("Data Source = Ozgur\\SQLEXPRESS;Initial Catalog = yazilim_Yapimi;Integrated Security = True");
         private void frmDetails_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'yazilim_YapimiDataSet.tbl_Event' table. You can move, or remove it, as needed.
             this.tbl_EventTableAdapter.Fill(this.yazilim_YapimiDataSet.tbl_Event);
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.tbl_EventTableAdapter.Fill(this.yazilim_YapimiDataSet.tbl_Event);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into tbl_Event (Date,Event) values (@p1,@p2)", connection);
+            command.Parameters.AddWithValue("@p1", tbDate.Text);
+            command.Parameters.AddWithValue("@p2", tbEvent.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+            clean();
+            MessageBox.Show("Event saved!");
+        }
+
+        void clean()
+        {
+            tbDate.Text = "";
+            tbEvent.Text = "";
+            tbDate.Focus();
+        }
+        private void btnCean_Click(object sender, EventArgs e)
+        {
+            clean();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selected = dataGridView1.SelectedCells[0].RowIndex;
+
+            tbDate.Text = dataGridView1.Rows[selected].Cells[0].Value.ToString();
+            tbEvent.Text = dataGridView1.Rows[selected].Cells[1].Value.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand commanddelte = new SqlCommand("Delete From tbl_Event Where Date=@d1", connection);
+            commanddelte.Parameters.AddWithValue("@d1", tbDate.Text);
+            commanddelte.ExecuteNonQuery();
+            connection.Close();
+            clean();
+            MessageBox.Show("Event deleted!");
         }
     }
 }
